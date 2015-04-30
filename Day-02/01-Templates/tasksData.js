68f819b47518237ac3ff3581d77232545347a2f6
tkmagesh <tkmagesh77@gmail.com>
2015-04-30 15:15:21 +0530

    Ajax Demo

--- Day-02/01-Templates/tasksData.js
+++ Day-02/01-Templates/tasksData.js
@@ -1,18 +1,20 @@
-var tasks = [
-	{
-		name : "Watch a movie",
-		isCompleted : false
-	},
-	{
-		name : "Learn jQuery",
-		isCompleted : true
-	},
-	{
-		name : "Explore HTML5",
-		isCompleted : false
-	},
-	{
-		name : "Master JavaScript",
-		isCompleted : true
-	}
-];
+{"list" : 
+	[
+		{
+			"name" : "Watch a movie",
+			"isCompleted" : false
+		},
+		{
+			"name" : "Learn jQuery",
+			"isCompleted" : true
+		},
+		{
+			"name" : "Explore HTML5",
+			"isCompleted" : false
+		},
+		{
+			"name" : "Master JavaScript",
+			"isCompleted" : true
+		}
+	]
+};
