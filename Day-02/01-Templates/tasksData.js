8572c6932b0e10ab73a81079e65640f5fec1a3ad
tkmagesh <tkmagesh77@gmail.com>
2015-04-30 23:43:06 +0530

    Templates

---
+++ Day-02/01-Templates/tasksData.js
@@ -0,0 +1,18 @@
+var tasks = [
+	{
+		name : "Watch a movie",
+		isCompleted : false
+	},
+	{
+		name : "Learn jQuery",
+		isCompleted : true
+	},
+	{
+		name : "Explore HTML5",
+		isCompleted : false
+	},
+	{
+		name : "Master JavaScript",
+		isCompleted : true
+	}
+];
